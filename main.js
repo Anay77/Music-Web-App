520e2a13274009b49dc93099da9dc15bf013d29b
Anay77 <76037278+Anay77@users.noreply.github.com>
2021-08-06 19:28:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+song1=loadSound("My Demons - Starset - (fan).mp3");
+song2=loadSound("believer Mp3 Imagine Dragons.mp3");
+scoreLeftWrist=0;
+scoreRightWrist=0;
 rightWristX=0;
 rightWristY=0;
 leftWristX=0;
@@ -24,7 +28,17 @@ function draw(){
     stroke("#2d015e");
     if(scoreLeftWrist > 0.2){
     circle(leftWristX,leftWristY,20);
-    song.isPlaying(volume);
+    song.stoping(song2);
+    }
+    if(song1=false){
+        song.play(song1);
+    }
+    if(score.rightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        song.stoping(song2);
+    }
+    if(song2=false){
+        song.play(song2);
     }
 }
 function play(){
@@ -35,8 +49,9 @@ function play(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         scoreLeftWrist=results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreLeftWrist = " + scoreLeftWrist);    
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX= " + leftWristX + "leftWristY" + leftWristY);
